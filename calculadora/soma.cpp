c8bf2db94fda1bafa063150b0601591693a657bd
Dreiv-coder <dreiv.dev@gmail.com>
2026-02-21 14:46:28 +0000

    Aula 1

---
+++ calculadora/soma.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    int a,b, soma;
+    cout << "quais os numeros que voce quer somar?\n";
+    cin >> a >> b;
+    soma = a+b;
+    cout << "a soma dos numeros é: " << soma << "\n";
+    return 0;
+}
---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    cout << "ola,mundo!\n"
+
+    return 0;
+}
